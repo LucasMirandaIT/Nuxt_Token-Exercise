da6fa8530130867773f9712f2ac153e85dfabd47
LucasMirandaIT <miranda.lucas@hotmail.com>
2025-08-14 22:04:31 -0300

    chore: add logs to /login endpoint

--- server/routes/api/login.post.ts
+++ server/routes/api/login.post.ts
@@ -5,11 +5,13 @@ export default defineEventHandler(async (event) => {
 
   try {
     const token = jwt.sign({ user: "admin" }, "secret", { expiresIn: "2m" }); // expires in 2 minutes
+    console.log("Token generated:", token);
 
     await setUserSession(event, {
       user: { name: "admin" },
       secure: { apiToken: token },
     });
+    console.log("Session set successfully");
   } catch (error) {
     throw createError({
       statusCode: 401,
