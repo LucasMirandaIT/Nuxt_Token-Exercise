14cb0d6e739a91cda19773ab2786a88501a57eab
LucasMirandaIT <miranda.lucas@hotmail.com>
2025-08-14 21:48:56 -0300

    fix: Add vercel preset to nitro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils'],
+  modules: ["nuxt-auth-utils"],
   runtimeConfig: {
     public: {
-      apiURL: 'https://689e4a983fed484cf876cea8.mockapi.io/api'
+      apiURL: "https://689e4a983fed484cf876cea8.mockapi.io/api",
     },
   },
   app: {
     head: {
-      title: 'Nuxt Auth Utils Example',
-    }
+      title: "Nuxt Auth Utils Example",
+    },
   },
   devServer: {
     port: 8080,
-    https: true
-  }
-})
+    https: true,
+  },
+  nitro: {
+    preset: "vercel",
+  },
+});
